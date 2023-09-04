c1e525622256cfddf351bb6a82e58269e082df97
br4hm <haehnlein.alexander@gmx.de>
2023-09-04 11:47:01 +0200

    Website Stage

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 // Typewriter
 
-var i = 0;
-var txt = 'klar. präzise. einfach.'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("typewriter").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
 
 
